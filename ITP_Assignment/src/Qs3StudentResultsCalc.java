44bf08074a77bbf5cd65e661cb3d288d6034db8f
KruleSufi <krulesufi@gmail.com>
2026-09-08 18:28:40 +0800

    Update and rename Q3StudentResultsCalc.java to Qs3StudentResultsCalc.java

--- ITP_Assignment/src/Qs3StudentResultsCalc.java
+++ ITP_Assignment/src/Qs3StudentResultsCalc.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q3StudentResultsCalc {
+public class Qs3StudentResultsCalc {
     public static void main(String[] args){
 
         //don't forget to remove the results in numbers (line 24)
